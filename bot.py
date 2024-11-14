bf2fe0bb4e5032ab86ed309652916cb367e9caa6
Whann Iñigo Eugenio <159619875+nengwha@users.noreply.github.com>
2024-11-14 20:19:24 +0800

    Discord Bot

---
+++ bot.py
@@ -0,0 +1,29 @@
+# Eugenio, Whann
+# November 14, 2024
+
+import discord
+import requests
+import json
+
+def get_meme():
+  response = requests.get('https://meme-api.com/gimme')
+  json_data = json.loads(response.text)
+  return json_data['url']
+
+class MyClient(discord.Client):
+  async def on_ready(self):
+    print('Logged on as {0}!'.format(self.user))
+
+  async def on_message(self, message):
+    if message.author == self.user:
+      return
+    if message.content.startswith('$hello'):
+      await message.channel.send('Memes for Life!')
+      await message.channel.send(get_meme())
+      
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = MyClient(intents=intents)
+client.run('Token') # Replace with your own token
